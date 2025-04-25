34a8c48a517f576bd3d20522345b63de3c8ecba0
n4mlz <n4mlz.dev@gmail.com>
2025-04-25 14:49:04 +0900

    :package: fontsource を導入

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,10 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        kiwimaru: ["Kiwi Maru", ...defaultTheme.fontFamily.sans],
+        kiwimaru: ["Kiwi Maru", "serif"],
       },
     },
   },
